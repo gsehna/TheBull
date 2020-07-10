2c986db8b1e7f29bd6cb758ba11d5736911c1dc6
Guilherme Sehna <guilhermesehna@hotmail.com>
2020-07-10 19:47:03 -0300

    Collision & Destroyables

---
+++ Assets/Scripts/DestroyableObject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyableObject : MonoBehaviour
+{
+    public ParticleSystem destructionParticles;
+    public float adrenalineGain;
+    public float scoreGain;
+
+    public void Destroy()
+    {
+        Instantiate(destructionParticles, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
